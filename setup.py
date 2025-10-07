0d8d700a64d9d4c8c19dabbeb8bb31a2242d3c50
dreamingcuriosity <altheahueteah@gmail.com>
2025-10-07 09:28:49 +0800

    Update to 0.4.0 (add OOP)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="cerona",
-    version="0.3.0",
+    version="0.4.0",
     author="ZaiperUnbound",
     author_email="altheahueteah@gmal.com",
     license="MIT",
